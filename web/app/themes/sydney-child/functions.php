3a83e28b1012851df3718de75b237151a55fbe1b
Andrew Exton <aexton@calpoly.edu>
2017-11-16 13:33:28 -0800

    more style additions to add pop

--- web/app/themes/sydney-child/functions.php
+++ web/app/themes/sydney-child/functions.php
@@ -5,3 +5,16 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 function enqueue_parent_styles(){
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
+
+/*
+ * Requiring latest news widget file from childtheme
+ */
+require get_stylesheet_directory() . "/widgets/affinity-latest-news.php";
+
+/*
+ * Registering new latest news widget
+ */
+function sydney_child_register_new_latest_news_widget(){
+  register_widget( 'Affinity_Latest_news' ); // Widget class name
+}
+add_action( 'widgets_init', 'sydney_child_register_new_latest_news_widget' );
